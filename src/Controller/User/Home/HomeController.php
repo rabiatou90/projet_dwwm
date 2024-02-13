42322c413da20d14df159166e326d4d6372cc213
Rabi <rabiatou.bah@gmail.com>
2024-02-13 18:17:05 +0100

    sprint2-ticket4-add:finition de l'espace admin

---
+++ src/Controller/User/Home/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\User\Home;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/user/home', name: 'user_home_index', methods:['GET'])]
+    public function index(): Response
+    {
+        return $this->render('/pages/user/home/index.html.twig');
+    }
+}
