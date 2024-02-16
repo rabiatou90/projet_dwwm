87fed32aae1bd3e84e9d523904328b88a7bed17c
Rabi <rabiatou.bah@gmail.com>
2024-02-16 12:13:05 +0100

    sprint3-ticket1-feat:Gestion des agences en place

---
+++ src/Controller/Admin/Agence/AgenceController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller\Admin\Agence;
+
+use App\Entity\Agence;
+use App\Form\AgenceFormType;
+use App\Repository\AgenceRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AgenceController extends AbstractController
+{
+    #[Route('/admin/agence/list', name: 'admin_agence_index', methods:['GET'])]
+    public function index(AgenceRepository $agenceRepository): Response
+    {
+
+        $agences = $agenceRepository->findAll();
+
+        return $this->render('pages/admin/agence/index.html.twig', [
+            "agences" => $agences
+        ]);
+    }
+
+    #[Route('/admin/agence/create', name: 'admin_agence_create', methods:['GET','POST'])]
+    public function create(Request $request, EntityManagerInterface $em): Response 
+    {
+        $agence = new Agence();
+
+        $form = $this->createForm(AgenceFormType::class, $agence);
+
+        $form->handleRequest($request);
+
+        if ( $form->isSubmitted() && $form->isValid() ) 
+        {
+            $em->persist($agence);
+            $em->flush();
+
+            $this->addFlash("success", "L'agence a été ajoutée.");
+
+            return $this->redirectToRoute('admin_agence_index');
+        }
+
+        return $this->render('pages/admin/agence/create.html.twig', [
+            "form" =>$form->createView()
+        ]);
+    }
+
+    #[Route('/admin/agence/{id}/edit', name: 'admin_agence_edit', methods:['GET', 'PUT'])]
+    public function edit(Agence $agence, Request $request, EntityManagerInterface $em): Response
+    {
+
+        $form = $this->createForm(AgenceFormType::class, $agence, [
+            "method" => "PUT"
+        ]);
+
+        $form->handleRequest($request);
+
+        if ( $form->isSubmitted() && $form->isValid() ) 
+        {
+            $em->persist($agence);
+            $em->flush();
+
+            $this->addFlash('success', "L'agence a été modifiée");
+
+            return $this->redirectToRoute("admin_agence_index");
+        }
+
+        return $this->render("pages/admin/agence/edit.html.twig", [
+            "form" => $form->createView()
+        ]);
+    }
+
+    #[Route('/admin/agence/{id}/delete', name: 'admin_agence_delete', methods:['DELETE'])]
+    public function delete (Agence $agence, Request $request, EntityManagerInterface $em): Response 
+    {
+        
+        if ( $this->isCsrfTokenValid('delete_agence_'.$agence->getId(), $request->request->get('csrf_token')) ) 
+        {
+            $em->remove($agence);
+            $em->flush();
+
+            $this->addFlash('success', "L'agence a été supprimée.");
+        }
+        
+        return $this->redirectToRoute('admin_agence_index');
+    }
+    }
+    
+
---
+++ src/Entity/Agence.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AgenceRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: AgenceRepository::class)]
+class Agence
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+    
+
+    #[Assert\NotBlank(message: "Le pays est obligatoire.")]
+    #[Assert\Length(
+        max: 255,
+        maxMessage: 'Le pays ne doit pas dépasser {{ limit }} caractères.',
+    )]
+    #[ORM\Column(length: 255)]
+    private ?string $pays = null;
+    
+
+    #[Assert\NotBlank(message: "La ville est obligatoire.")]
+    #[Assert\Length(
+        max: 255,
+        maxMessage: 'La ville ne doit pas dépasser {{ limit }} caractères.',
+    )]
+    #[ORM\Column(length: 255)]
+    private ?string $ville = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $libelle = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPays(): ?string
+    {
+        return $this->pays;
+    }
+
+    public function setPays(?string $pays): static
+    {
+        $this->pays = $pays;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(?string $ville): static
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(?string $libellé): static
+    {
+        $this->libelle = $libellé;
+
+        return $this;
+    }
+}
